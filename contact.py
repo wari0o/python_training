2962186daad43d3e672a0af9e25eaba0f5cc051a
v,suvorov <v.suvorov@devcaz.com>
2020-07-16 20:49:51 +0300

    task3

---
+++ contact.py
@@ -0,0 +1,27 @@
+class Contact:
+    def __init__(self, firstname, middlename, nickname, lastname, title, company, address, homenumber, mobilenumber, worknumber, faxnumber, email1, email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear, group, address2, phone2, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.nickname = nickname
+        self.lastname = lastname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.homenumber = homenumber
+        self.mobilenumber = mobilenumber
+        self.worknumber = worknumber
+        self.faxnumber = faxnumber
+        self.email1 = email1
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.group = group
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
