25bcf4c5432f38f2276d603e407c980887e17177
v,suvorov <v.suvorov@devcaz.com>
2020-07-24 01:08:31 +0300

    Выделен класс для управления сессиями

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,8 +11,8 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.add_contact(Contact(firstname="tuserfirst", middlename="tusermiddle", nickname="tusernick", lastname="tuserlast", title="contact", company="test", address="Moscow", homenumber="123",
                      mobilenumber="8915", worknumber="8916", faxnumber="8917", email1="tuser@mail.ru", email2="tuser@mail.com", email3="tuser@mail.rucom", homepage="tuser.ru", bday="2",
                      bmonth="January", byear="1990", aday="16", amonth="July", ayear="2020", group="[none]", address2="Russia", phone2="8920", notes="none"))
-    app.logout()
+    app.session.logout()
