b4f8005c30c5a56e6c683568406d1deda8799500
v,suvorov <v.suvorov@devcaz.com>
2020-07-30 16:01:58 +0300

    task10

--- fixture/contact.py
+++ fixture/contact.py
@@ -90,7 +90,8 @@ class ContactHelper:
 
     def open_home_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not (wd.current_url.endswith("/addressbook.php") and len(wd.find_elements_by_xpath("//input[@value='Delete']")) > 0):
+            wd.find_element_by_link_text("home").click()
 
     def count(self):
         wd = self.app.wd
