157ede88350a4e4ff9b077460f28849f09553410
v,suvorov <v.suvorov@devcaz.com>
2020-08-12 02:15:45 +0300

    task14v2 add random

--- test/test_verification_contact.py
+++ test/test_verification_contact.py
@@ -1,9 +1,11 @@
 import re
+from random import randrange
 
 
 def test_verification_contact(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    index = randrange(len(app.contact.get_contact_list()))
+    contact_from_home_page = app.contact.get_contact_list()[index]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_home_page.lastname == contact_from_home_page.lastname
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
     assert contact_from_home_page.address == contact_from_edit_page.address
